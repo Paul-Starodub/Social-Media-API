c00d527e00b1467fa47f743d77b70f0c65a63e3a
Paul Starodub <amorallex@gmail.com>
2023-05-15 21:51:33 +0300

    Updated models.py

--- post/models.py
+++ post/models.py
@@ -1,3 +1,22 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """Post model"""
+    create_date = models.DateField(auto_now=True)
+    content = models.CharField()
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name="posts",
+    )
+    commentary = models.CharField(max_length=250, null=True, blank=True)
+    count_like = models.PositiveIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.content[:25]
+
+    class Meta:
+        ordering = ["-create_date"]
+
