48e339f34ca9001c65db3dd67ba55621758bdb67
Paul Starodub <amorallex@gmail.com>
2023-05-17 08:28:14 +0300

    Updated post/models.py with like

--- post/models.py
+++ post/models.py
@@ -1,22 +1,68 @@
+from __future__ import annotations
+import os
+import uuid
+
 from django.conf import settings
+from django.contrib.contenttypes.fields import (
+    GenericForeignKey,
+    GenericRelation,
+)
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.utils.text import slugify
+
+
+def movie_image_file_path(instance: Post, filename: str) -> str:
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.content[:10])}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/movies/posts/", filename)
+
+
+class Like(models.Model):
+    """Like model"""
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="likes",
+        on_delete=models.CASCADE,
+    )
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
 
 
 class Post(models.Model):
     """Post model"""
+
     create_date = models.DateField(auto_now=True)
-    content = models.CharField()
+    content = models.CharField(max_length=280)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.PROTECT,
         related_name="posts",
     )
-    commentary = models.CharField(max_length=250, null=True, blank=True)
-    count_like = models.PositiveIntegerField(null=True, blank=True)
+    likes = GenericRelation(Like)
+    image = models.ImageField(
+        null=True, blank=True, upload_to=movie_image_file_path
+    )
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.content[:25]
+
+    @property
+    def total_likes(self):
+        return self.likes.count()
 
     class Meta:
         ordering = ["-create_date"]
 
+
+class Commentary(models.Model):
+    commentary = models.CharField(max_length=350)
+    post = models.ForeignKey(
+        Post, on_delete=models.PROTECT, related_name="commentaries"
+    )
+
+    def __str__(self) -> str:
+        return self.commentary[:14]
