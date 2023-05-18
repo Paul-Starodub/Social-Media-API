1df83818f20ff5fdf96e948ff851deb8234dbc28
Paul Starodub <amorallex@gmail.com>
2023-05-18 14:33:56 +0300

    Updated post/views.py

--- post/views.py
+++ post/views.py
@@ -1,6 +1,7 @@
 from typing import Type, Optional
 
-from rest_framework import status
+from django.db.models import QuerySet
+from rest_framework import status, generics
 from rest_framework.decorators import action
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -14,7 +15,6 @@ from post.serializers import (
     CommentarySerializer,
     PostListSerializer,
     LikePostSerializer,
-    PostDetailSerializer,
     CommentaryRemoveSerializer,
 )
 
@@ -22,17 +22,28 @@ from post.serializers import (
 class PostViewSet(ModelViewSet):
     """Post CRUD endpoints"""
 
-    queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    def get_queryset(self) -> QuerySet[Post]:
+        queryset = Post.objects.all()
+        hashtag = self.request.query_params.get("hashtag")
+
+        if hashtag:
+            queryset = queryset.filter(content__icontains=hashtag)
+
+        return queryset
+
     def get_serializer_class(self) -> Type[Serializer]:
-        if self.action == "list":
+        if self.action in ("list", "retrieve"):
             return PostListSerializer
 
-        if self.action == "retrieve":
-            return PostDetailSerializer
-
         return super().get_serializer_class()
+
+    def perform_create(
+        self,
+        serializer: Serializer,
+    ) -> None:
+        serializer.save(user=self.request.user)
 
     @action(
         methods=["POST"],
@@ -45,7 +56,7 @@ class PostViewSet(ModelViewSet):
 
         obj = self.get_object()
         services.add_like(obj, request.user)
-        return Response("You like this post", status=status.HTTP_200_OK)
+        return Response("You liked this post", status=status.HTTP_200_OK)
 
     @action(
         methods=["POST"],
@@ -71,12 +82,15 @@ class PostViewSet(ModelViewSet):
     ) -> Response:
         """Creates comment"""
 
-        obj = self.get_object()
+        post = self.get_object()
         commentary = request.data.get("commentary")
-        comm = Commentary.objects.create(commentary=commentary, post_id=obj.id)
+        comm = Commentary.objects.create(
+            commentary=commentary, post=post, user=self.request.user
+        )
         serializer = self.get_serializer(comm, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     @action(
@@ -88,19 +102,50 @@ class PostViewSet(ModelViewSet):
     def remove_all_comments(
         self, request: Request, pk: Optional[int] = None
     ) -> Response:
-        """Deletes all commentaries"""
+        """Deletes all own commentaries"""
 
-        commentaries = self.get_object().commentaries.all()
+        commentaries = self.get_object().commentaries.filter(
+            user_id=self.request.user.id
+        )
         if len(commentaries) > 0:
+            commentaries.delete()
             return Response(
-                "All commentaries deleted", status=status.HTTP_200_OK
+                "All my commentaries deleted", status=status.HTTP_200_OK
             )
 
         return Response("You have no comments", status=status.HTTP_200_OK)
 
+    @action(
+        methods=["GET"],
+        detail=False,
+        url_path="liked",
+    )
+    def liked_posts(
+        self, request: Request, pk: Optional[int] = None
+    ) -> Response:
+        """Liked posts"""
 
-class CommentaryViewSet(ModelViewSet):
+        queryset = self.filter_queryset(self.get_queryset()).filter()
+        like_qs = [
+            _
+            for _ in queryset
+            if list(_.likes.values("user_id")) != []
+            if self.request.user.id
+            in sum(list(_.likes.values_list("user_id")), ())
+        ]
+        page = self.paginate_queryset(like_qs)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(like_qs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CommentaryViewSet(generics.ListAPIView):
     """Commentary CRUD endpoints"""
 
-    queryset = Commentary.objects.all()
     serializer_class = CommentarySerializer
+
+    def get_queryset(self) -> QuerySet[Commentary]:
+        return Commentary.objects.filter(user_id=self.request.user.id)
