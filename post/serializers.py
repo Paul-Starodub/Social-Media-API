011826d86d8305ba9329528646ccfb276006320e
Paul Starodub <amorallex@gmail.com>
2023-05-18 09:06:43 +0300

    Updated post/serializers.py

--- post/serializers.py
+++ post/serializers.py
@@ -3,7 +3,37 @@ from rest_framework import serializers
 from post.models import Post, Commentary
 
 
+class CommentarySerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        many=False,
+        read_only=True,
+        slug_field="email",
+    )
+
+    class Meta:
+        model = Commentary
+        fields = (
+            "id",
+            "post",
+            "user",
+            "commentary",
+        )
+        read_only_fields = ("user", "post")
+
+
+class CommentaryRemoveSerializer(CommentarySerializer):
+    class Meta:
+        model = Commentary
+        fields = ("id",)
+
+
 class PostSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        many=False,
+        read_only=True,
+        slug_field="email",
+    )
+
     class Meta:
         model = Post
         fields = (
@@ -17,18 +47,7 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class PostListSerializer(PostSerializer):
-    user = serializers.SlugRelatedField(
-        many=False, read_only=True, slug_field="email"
-    )
-
-
-class PostDetailSerializer(PostSerializer):
-    commentaries = serializers.SlugRelatedField(
-        many=True, read_only=True, slug_field="commentary"
-    )
-    user = serializers.SlugRelatedField(
-        many=False, read_only=True, slug_field="email"
-    )
+    commentaries = CommentarySerializer(many=True, read_only=True)
 
     class Meta:
         model = Post
@@ -37,28 +56,13 @@ class PostDetailSerializer(PostSerializer):
             "create_date",
             "content",
             "total_likes",
-            "commentaries",
             "image",
             "user",
+            "commentaries",
         )
 
 
 class LikePostSerializer(PostSerializer):
     class Meta:
         model = Post
-        fields = ("id",)
-
-
-class CommentarySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Commentary
-        fields = (
-            "id",
-            "commentary",
-        )
-
-
-class CommentaryRemoveSerializer(CommentarySerializer):
-    class Meta:
-        model = Commentary
         fields = ("id",)
