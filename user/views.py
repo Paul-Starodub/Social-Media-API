859c12f4b9f896df13b5c48779125a4fdcaf3b9a
Paul Starodub <amorallex@gmail.com>
2023-05-19 09:55:41 +0300

    Updated user/views.py

--- user/views.py
+++ user/views.py
@@ -1,18 +1,21 @@
 from django.contrib.auth import get_user_model
 from django.db.models import QuerySet
-from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework import generics, viewsets
+from rest_framework.serializers import Serializer
 
-
-from user.models import User
-from user.serializers import UserSerializer
+from user.models import User, UserFollowing
+from user.permissions import IsAuthenticatedOrAnonymous
+from user.serializers import (
+    UserSerializer,
+    FollowingSerializer,
+)
 
 
 class CreateUserView(generics.ListCreateAPIView):
     """Create new user"""
 
     serializer_class = UserSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticatedOrAnonymous,)
 
     def get_queryset(self) -> QuerySet:
         queryset = get_user_model().objects.all()
@@ -31,3 +34,20 @@ class ManageUserView(generics.RetrieveUpdateDestroyAPIView):
 
     def get_object(self) -> User:
         return self.request.user
+
+
+class RetrieveUserView(generics.RetrieveAPIView):
+    """Retrieve user witch already login"""
+
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class UserFollowingViewSet(viewsets.ModelViewSet):
+    """Following users"""
+
+    serializer_class = FollowingSerializer
+    queryset = UserFollowing.objects.all()
+
+    def perform_create(self, serializer: Serializer) -> None:
+        serializer.save(user_id=self.request.user)
