13cd6364bcc2ad945b059e60ea2d3c1b3c0ab7d9
Paul Starodub <amorallex@gmail.com>
2023-05-19 17:56:45 +0300

    Updated user/permissions.py

--- user/permissions.py
+++ user/permissions.py
@@ -1,5 +1,4 @@
 from django.views import View
-from rest_framework import permissions
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 from rest_framework.request import Request
 
@@ -20,12 +19,12 @@ class IsAuthenticatedOrAnonymous(BasePermission):
 
 
 class IsOwnerFollowing(BasePermission):
+    """Permissions for users owners"""
+
     def has_object_permission(
         self, request: Request, view: View, obj: UserFollowing
     ) -> bool:
-        """Permissions for users owners"""
-
-        if request.method in permissions.SAFE_METHODS:
+        if request.method in SAFE_METHODS:
             return True
 
         return obj.user_id == request.user
