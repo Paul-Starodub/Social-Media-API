157e040aabefdf0ba1c002b588051238988f5245
Paul Starodub <amorallex@gmail.com>
2023-05-19 14:05:29 +0300

    Updated user/serializers.py

--- user/serializers.py
+++ user/serializers.py
@@ -3,11 +3,34 @@ import datetime
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
+from rest_framework.validators import UniqueTogetherValidator
 
-from user.models import User
+from user.models import User, UserFollowing
+
+
+class FollowingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserFollowing
+        fields = ("id", "user_id", "following_user_id", "created")
+        read_only_fields = ("created",)
+        validators = [
+            UniqueTogetherValidator(
+                queryset=UserFollowing.objects.all(),
+                fields=["user_id", "following_user_id"],
+            )
+        ]
+
+
+class FollowersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserFollowing
+        fields = ("id", "user_id", "created")
 
 
 class UserSerializer(serializers.ModelSerializer):
+    following = FollowingSerializer(many=True, read_only=True)
+    followers = FollowersSerializer(many=True, read_only=True)
+
     class Meta:
         model = get_user_model()
         fields = (
@@ -19,16 +42,20 @@ class UserSerializer(serializers.ModelSerializer):
             "biography",
             "date_of_birth",
             "profile_image",
+            "following",
+            "followers",
         )
         read_only_fields = ("is_staff",)
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data: dict) -> User:
         """Create a new user with encrypted password and return it"""
+
         return get_user_model().objects.create_user(**validated_data)
 
     def update(self, instance: User, validated_data: dict) -> User:
         """Update a user, set the password correctly and return it"""
+
         password = validated_data.pop("password", None)
         user = super().update(instance, validated_data)
         if password:
@@ -44,6 +71,8 @@ class UserSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("You must indicate your age!")
 
         if value > datetime.date.today() - datetime.timedelta(weeks=260):
-            raise serializers.ValidationError("You must be at least 5 years old!")
+            raise serializers.ValidationError(
+                "You must be at least 5 years old!"
+            )
 
         return value
